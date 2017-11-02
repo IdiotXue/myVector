e89a67d86589a886a012016e3a57ed73dd852925
zero <xue.zl@qq.com>
2017-11-02 17:14:10 +0800

    v2.0 add iterator nested class

--- include/Vector.h
+++ include/Vector.h
@@ -12,6 +12,70 @@ namespace myvector
 template <typename Object>
 class Vector
 {
+  private:
+    int m_size;      //大小,与m_capacity声明顺序不能换
+    int m_capacity;  //容量
+    Object *m_pObjs; //数组
+    const static int SPARE_CAPACITY = 16;
+
+  public:
+    class const_iterator
+    {
+      protected:
+        Object *m_pCur;
+        /**
+         * 不允许除Vector的其他类通过指针构造迭代器,只在begin和end中调用
+         * 但该函数是protected，无法被Vector反问，需要额外想办法
+         */
+        const_iterator(Object *p) : m_pCur(p) {}
+        /*不同数据结构的检索方式不同，比如vector和list不同，所以封装于一个函数*/
+        Object &retrieve() const { return *m_pCur; }
+        friend class Vector<Object>; //友元声明，使Vector可以反问构造函数
+
+      public:
+        const_iterator() : m_pCur(nullptr) {} //默认初始化为NULL，避免野指针问题
+        const Object &operator*() const { return retrieve(); }
+        /*前置++*/
+        const_iterator &operator++()
+        {
+            ++m_pCur;
+            return *this;
+        }
+        /*后置++*/
+        const_iterator operator++(int)
+        {
+            const_iterator old = *this;
+            ++(*this);
+            return old;
+        }
+        bool operator==(const const_iterator &rhs) const { return m_pCur == rhs.m_pCur; }
+        bool operator!=(const const_iterator &rhs) const { return !(*this == rhs); }
+    };
+    class iterator : public const_iterator
+    {
+      protected:
+        iterator(Object *p) : const_iterator(p) {}
+        friend class Vector<Object>;
+
+      public:
+        iterator() : const_iterator() {}
+        Object &operator*() { return const_iterator::retrieve(); }
+        const Object &operator*() const { return this->retrieve(); } //或许可以不用
+        /*前置++*/
+        iterator &operator++()
+        {
+            ++this->m_pCur;
+            return *this;
+        }
+        /*后置++*/
+        iterator operator++(int)
+        {
+            iterator old = *this;
+            ++(*this);
+            return old;
+        }
+    };
+
   public:
     explicit Vector(int initSize = 0) : m_size(initSize), m_capacity(m_size + SPARE_CAPACITY)
     {
@@ -39,6 +103,12 @@ class Vector
         }
         return *this;
     }
+
+    const_iterator begin() const { return const_iterator(m_pObjs); }
+    const_iterator end() const { return const_iterator(m_pObjs + m_size); }
+    iterator begin() { return iterator(m_pObjs); }
+    iterator end() { return iterator(m_pObjs + m_size); }
+
     Object &operator[](int index) //修改
     {
         if (index >= m_size)
@@ -85,12 +155,6 @@ class Vector
         --m_size;
     }
     const Object &back() const { return m_pObjs[m_size - 1]; }
-
-  private:
-    int m_size;      //大小,与m_capacity声明顺序不能换
-    int m_capacity;  //容量
-    Object *m_pObjs; //数组
-    const static int SPARE_CAPACITY = 16;
 };
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "Vector.h"
+#include <string>
 using namespace std;
+using namespace myvector;
 
 int main(int argc, char const *argv[])
 {
     for (int i = 0; i < argc; ++i)
         cout << argv[i] << " ";
     cout << endl;
-    myvector::Vector<int> iTemp(3);
+    Vector<int> iTemp(3);
     // try
     // {
     //     iTemp[4] = 0;
@@ -28,4 +30,26 @@ int main(int argc, char const *argv[])
     for (int i = 0; i < iTemp2.size(); ++i)
         cout << iTemp2[i] << " ";
     cout << endl;
+    Vector<int>::const_iterator const_itr;
+    for (const_itr = iTemp.begin(); const_itr != iTemp.end(); ++const_itr)
+    {
+        // *const_itr = '666';
+        cout << *const_itr << " ";
+    }
+    cout << endl;
+
+    Vector<string> iStr(3);
+    iStr[0] = "hello";
+    iStr[1] = "hi";
+    iStr[2] = "nihao";
+    iStr.push_back("gaoxiao");
+    Vector<string>::iterator itr;
+    for (itr = iStr.begin(); itr != iStr.end(); itr++)
+    {
+        // *itr = "test";
+        cout << *itr << " ";
+    }
+    cout << *itr <<endl;
+    cout << endl;
+    cout << iStr[0] << endl;
 }
